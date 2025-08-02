0120bd8dfccdb2e318acd4394d300e28353a310d
hjkim27 <hjkim27.dev@gmail.com>
2025-08-03 01:35:18 +0900

    [Chore] 기본 검색 객체 추가

---
+++ src/main/java/hjkim27/dev/bean/common/DefaultSearch.java
@@ -0,0 +1,56 @@
+package hjkim27.dev.bean.common;
+
+import lombok.*;
+
+/**
+ * <pre>
+ *     기본 검색 객체
+ * </pre>
+ *
+ * @since 2025.08
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DefaultSearch {
+
+    private Integer currentPage = 1;    // 현재페이지
+    private Integer pageSize = 20;      // 한 페이지에 보일 row 수
+    private Integer pageInterval = 5;   // 하단 페이징 최대 출력 수
+    private Integer totalCount = 0;
+
+    private String searchKey;   // 검색 키
+    private String searchValue; // 검색 값
+    private String searchType;  // 검색 구분 값
+
+    private Integer columnIdx;  // 정렬 column index
+    private Boolean desc;       // 오름/내림차순 확인
+
+    private Boolean usePaging = true;
+
+    private Integer sid;        // sid 검색
+
+    public int getLimit() {
+        return pageSize;
+    }
+
+    public int getOffset() {
+        return pageSize * (currentPage - 1);
+    }
+
+    public int getLastPage() {
+        if (totalCount == 0) return 1;
+        return (int) Math.ceil(totalCount / pageSize);
+    }
+
+    public int getMinPage() {
+        return ((currentPage - 1) / pageInterval) * pageInterval + 1;
+    }
+
+    public int getMaxPage() {
+        return Math.min(getMinPage() + pageInterval - 1, getLastPage());
+    }
+
+}

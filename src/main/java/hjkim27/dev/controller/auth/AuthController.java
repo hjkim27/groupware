73d4d059df25b1e6410ceb6fe77f7330d60f5865
hjkim27 <hjkim27.dev@gmail.com>
2025-08-11 21:55:28 +0900

    [Refactor] 인증 controller 분리
    - view, rest

--- src/main/java/hjkim27/dev/controller/auth/AuthController.java
+++ src/main/java/hjkim27/dev/controller/auth/AuthController.java
@@ -1,32 +1,22 @@
 package hjkim27.dev.controller.auth;
 
-import hjkim27.dev.bean.user.vo.UserRequestLogin;
-import hjkim27.dev.bean.user.vo.UserResponseLogin;
-import hjkim27.dev.enumeration.MessageEnum;
-import hjkim27.dev.exception.ClientException;
-import hjkim27.dev.exception.WrongParamException;
-import hjkim27.dev.service.UserService;
 import hjkim27.dev.util.auth.AuthUtil;
-import hjkim27.dev.util.common.ValidParamUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <pre>
- *     로그인/로그아웃 관리
+ *     인증관련 view controller
+ *     - 로그인, 로그아웃 / 비밀번호 찾기 / 인증
  * </pre>
+ *
+ * @since 2025.08
  */
 @Slf4j
 @Controller
@@ -34,9 +24,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final String VIEW_FOLDER = "auth/";
-
-    private final UserService userService;
+    private final String VIEW_FOLDER = "auth";
 
     /**
      * <pre>
@@ -50,7 +38,7 @@ public class AuthController {
     public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
         log.info("[{}] {}", request.getMethod(), request.getRequestURI());
 
-        ModelAndView mav = new ModelAndView(VIEW_FOLDER + "login/user");
+        ModelAndView mav = new ModelAndView(VIEW_FOLDER + "/login/user");
         if (AuthUtil.isLogin(request)) {
             // todo 메인페이지로
         }
@@ -69,7 +57,7 @@ public class AuthController {
     public ModelAndView loginAdmin(HttpServletRequest request, HttpServletResponse response) {
         log.info("[{}] {}", request.getMethod(), request.getRequestURI());
 
-        ModelAndView mav = new ModelAndView(VIEW_FOLDER + "login/admin");
+        ModelAndView mav = new ModelAndView(VIEW_FOLDER + "/login/admin");
         if (AuthUtil.isAdmin(request)) {
             // todo 관리자 메인 페이지로
         }
@@ -98,51 +86,4 @@ public class AuthController {
 
         return new ModelAndView(new RedirectView(request.getContextPath() + "/auth/login"));
     }
-
-    /**
-     * <pre>
-     *     로그인 동작
-     * </pre>
-     *
-     * @param request
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
-    public Map<String, Object> loginPost(
-            HttpServletRequest request, HttpServletResponse response,
-            @ModelAttribute UserRequestLogin info
-    ) {
-        log.info("[{}] {}", request.getMethod(), request.getRequestURI());
-
-        Map<String, Object> map = new HashMap<String, Object>();
-        MessageEnum messageEnum = null;
-
-        try {
-            // 필수 정보 입력 확인
-            if (!ValidParamUtil.isValidParam(info)) {
-                messageEnum = MessageEnum.INVALID_PARAMETER;
-                throw new WrongParamException(messageEnum.getMessage());
-            }
-            // 로그인 정보 확인
-            UserResponseLogin responseLogin = userService.loginCheck(info);
-            if (responseLogin == null) {
-                messageEnum = MessageEnum.USER_NOT_FOUND;
-                throw new ClientException(messageEnum.getMessage());
-            }
-            // 로그인 성공
-            AuthUtil.setLogin(request, response, responseLogin);
-            messageEnum = MessageEnum.SUCCESS;
-
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        } finally {
-            if (messageEnum == null) {
-                messageEnum = MessageEnum.INVALID_AUTHENTICATION;
-            }
-            map.putAll(messageEnum.getMessageInfo());
-        }
-        return map;
-    }
-
 }
---
+++ src/main/java/hjkim27/dev/controller/auth/AuthRestController.java
@@ -0,0 +1,87 @@
+package hjkim27.dev.controller.auth;
+
+import hjkim27.dev.bean.user.vo.UserRequestLogin;
+import hjkim27.dev.bean.user.vo.UserResponseLogin;
+import hjkim27.dev.enumeration.MessageEnum;
+import hjkim27.dev.exception.ClientException;
+import hjkim27.dev.exception.WrongParamException;
+import hjkim27.dev.service.UserService;
+import hjkim27.dev.util.auth.AuthUtil;
+import hjkim27.dev.util.common.ValidParamUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <pre>
+ *     인증 관련 rest controller
+ *     - 로그인 / 비밀번호 찾기 / 중복확인
+ * </pre>
+ *
+ * @since 2025.08
+ */
+@Slf4j
+@Controller
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthRestController {
+
+    private final UserService userService;
+
+    /**
+     * <pre>
+     *     로그인 동작
+     * </pre>
+     *
+     * @param request
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
+    public Map<String, Object> loginPost(
+            HttpServletRequest request, HttpServletResponse response,
+            @ModelAttribute UserRequestLogin info
+    ) {
+        log.info("[{}] {}", request.getMethod(), request.getRequestURI());
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        MessageEnum messageEnum = null;
+
+        try {
+            // 필수 정보 입력 확인
+            if (!ValidParamUtil.isValidParam(info)) {
+                messageEnum = MessageEnum.INVALID_PARAMETER;
+                throw new WrongParamException(messageEnum.getMessage());
+            }
+            // 로그인 정보 확인
+            UserResponseLogin responseLogin = userService.loginCheck(info);
+            if (responseLogin == null) {
+                messageEnum = MessageEnum.USER_NOT_FOUND;
+                throw new ClientException(messageEnum.getMessage());
+            }
+            // 로그인 성공
+            AuthUtil.setLogin(request, response, responseLogin);
+            messageEnum = MessageEnum.SUCCESS;
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        } finally {
+            if (messageEnum == null) {
+                messageEnum = MessageEnum.INVALID_AUTHENTICATION;
+            }
+            map.putAll(messageEnum.getMessageInfo());
+        }
+        return map;
+    }
+
+
+}

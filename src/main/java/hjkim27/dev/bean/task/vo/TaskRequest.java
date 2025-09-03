3e5a8215689ee224874c39514f2b8a4d8f1ab409
hjkim27 <hjkim27.dev@gmail.com>
2025-09-03 23:59:03 +0900

    [Feat] task 관련 클래스 추가
    - blank class

---
+++ src/main/java/hjkim27/dev/bean/task/vo/TaskRequest.java
@@ -0,0 +1,20 @@
+package hjkim27.dev.bean.task.vo;
+
+import lombok.*;
+
+
+/**
+ * <pre>
+ *     업무 추가
+ * </pre>
+ *
+ * @since 2025.09
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskRequest {
+
+}
---
+++ src/main/java/hjkim27/dev/bean/task/vo/TaskResponse.java
@@ -0,0 +1,23 @@
+package hjkim27.dev.bean.task.vo;
+
+import hjkim27.dev.enumeration.TaskStatusEnum;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * <pre>
+ *     업무 조회
+ * </pre>
+ *
+ * @since 2025.09
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskResponse {
+
+}
